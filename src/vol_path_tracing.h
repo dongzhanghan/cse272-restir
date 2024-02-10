039fbbfc3984b3ddce60a7e31567153742f064d0
Skyler <yiz249@ucsd.edu>
2024-02-10 15:08:14 -0800

    hw2.1

--- src/vol_path_tracing.h
+++ src/vol_path_tracing.h
@@ -1,13 +1,35 @@
 #pragma once
-
+#include "scene.h"
+#include "pcg.h"
 // The simplest volumetric renderer: 
 // single absorption only homogeneous volume
 // only handle directly visible light sources
 Spectrum vol_path_tracing_1(const Scene &scene,
                             int x, int y, /* pixel coordinates */
                             pcg32_state &rng) {
-    // Homework 2: implememt this!
-    return make_zero_spectrum();
+   int w = scene.camera.width, h = scene.camera.height;
+    Vector2 screen_pos((x ) / w,
+                       (y ) / h);
+    Ray ray = sample_primary(scene.camera, screen_pos);
+    //disable ray differentials
+    RayDifferential ray_diff = RayDifferential{Real(0), Real(0)};
+
+    std::optional<PathVertex> vertex_ = intersect(scene, ray, ray_diff);
+    if (!vertex_) {
+        return make_zero_spectrum();
+    }
+    Spectrum radiance = make_zero_spectrum();
+    PathVertex vertex = *vertex_;
+    Spectrum sigma_a = get_sigma_a(scene.media[vertex.exterior_medium_id], vertex.position);
+    Real t = length(vertex.position - ray.org);
+    Spectrum transmittance = exp(- sigma_a * t);
+    Spectrum Le = make_zero_spectrum();
+    if(is_light(scene.shapes[vertex.shape_id])) {
+        Le = emission(vertex, -ray.dir, scene);
+    }
+    radiance = Le * transmittance;
+    return radiance;
+
 }
 
 // The second simplest volumetric renderer: 
